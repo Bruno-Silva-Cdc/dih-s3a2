f11c1a7bb6b0c6bc8aa1d1c9bdf4c4031559031b
romeuic <casarottori@gmail.com>
2021-11-03 21:18:22 -0300

    Adiciona arquivos iniciais

---
+++ app.js
@@ -0,0 +1,12 @@
+const mostrar = texto => {
+  const display = document.querySelector('#display')
+  if (display) {
+    display.innerHTML += `<p>${texto}</p>`
+  }
+}
+
+const logaDiferente = texto => console.log(texto)
+
+
+mostrar('Olá! :)')
+mostrar('321')
